8e2108078ba1cf57fd76f7f4dbf3e6c4c33f6240
tropnepod <59154286+tropnepod@users.noreply.github.com>
2020-05-12 15:49:04 -0700

    add acme accounting drill

---
+++ AcmeAccounting/AcmeAccounting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.WriteLine("Remember, we're \"accoounting\" on you!");
+        Console.ReadLine();
+        }
+    }
+

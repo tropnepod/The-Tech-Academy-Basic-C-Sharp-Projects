5a8a1a5ae91e0d6390f4bcd3db7fb8f06ac334d6
tropnepod <59154286+tropnepod@users.noreply.github.com>
2020-05-14 14:04:57 -0700

    add daily report assignment

---
+++ DailyReportAssignment/DailyReportAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyReportAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What course are you on?");
+            string currentCourse = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            string currentPageNum = Console.ReadLine();
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\"");
+            string needHelp = Console.ReadLine();
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string additionalFeedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string hoursStudied = Console.ReadLine();
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            //Console.WriteLine(currentCourse + " : " + currentPageNum + " : " + needHelp + " : " + feedback + " : " + additionalFeedback + " : " + hoursStudied);
+            Console.ReadLine();
+        }
+    }
+}

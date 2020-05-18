7adab4daba6fd9cb3fa858ba51f1a9c1dcc9b0ab
tropnepod <59154286+tropnepod@users.noreply.github.com>
2020-05-18 16:40:10 -0700

    add class assignment 2 files

---
+++ ClassAssignment2/ClassAssignment2/MathOps.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment2
+{
+    public class MathOps
+    {
+        public static double SomeMath(int userNumber)
+        {
+            return Math.Pow(userNumber, 5);
+        }
+
+        public static double SomeMath(double userNumber2)
+        {
+            int result = Convert.ToInt32(Math.Sqrt(userNumber2));
+            return result;
+        }
+
+        public static int SomeMath(string userNumber3, out string failure)
+        {
+            bool isValid = Int32.TryParse(userNumber3, out int result);
+            failure = "";
+            if (isValid)
+            {
+                int number = Convert.ToInt32(userNumber3);
+                int operation = Convert.ToInt32((number * 145) - Math.Pow(number, 2));
+                return operation;
+            }
+            else
+            {
+                failure = "There is no spoon";
+                return result;
+            }
+            
+            
+            
+
+        }
+    }
+}
---
+++ ClassAssignment2/ClassAssignment2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer to do some stuff to: ");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The result of the operations is: " + MathOps.SomeMath(userNumber));
+
+            Console.WriteLine("Enter a decimal to do some stuff: ");
+            double userNumber2 = Convert.ToDouble(Console.ReadLine());
+
+            
+            Console.WriteLine("The result of the operation is: " + MathOps.SomeMath(userNumber2));
+
+            Console.WriteLine("Please enter an integer to do some stuff to: ");
+            string userNumber3 = Console.ReadLine();
+            string failure = "";
+            int result = MathOps.SomeMath(userNumber3, out failure);
+
+
+            if (failure != "")
+            {
+                Console.WriteLine("The conversion to integer was impossible...");
+            }
+            else
+            {
+                Console.WriteLine("The result of the operation is: " + result);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

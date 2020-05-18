6a3167aa2402867caadaf1ba348a21ee7b5e07b2
tropnepod <59154286+tropnepod@users.noreply.github.com>
2020-05-18 11:48:41 -0700

    add class assignment files

---
+++ ClassAssignment/ClassAssignment/MathStuff.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    public class MathStuff
+    {
+        public static int TripleDouble(int userNumber)
+        {
+            return ((3 * userNumber) * 2);
+        }
+
+        public static int SecretMath(int userNumber)
+        {
+            return (((userNumber + 167) * 23) - 67);
+        }
+
+        public static int ThirdPower(int userNumber)
+        {
+            return (userNumber ^ 3);
+        }
+    }
+}
---
+++ ClassAssignment/ClassAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a whole number to do some math operations on: ");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Op 1: " + MathStuff.TripleDouble(userNumber));
+            Console.WriteLine("Op 2: " + MathStuff.SecretMath(userNumber));
+            Console.WriteLine("Op 3: " + MathStuff.ThirdPower(userNumber));
+
+            Console.ReadLine();
+
+        }
+    }
+}

212a8ca0407ed95bda06865a49667d74e14f1441
tropnepod <59154286+tropnepod@users.noreply.github.com>
2020-05-18 17:24:36 -0700

    add class assignment 3 files

---
+++ ClassAssignment3/ClassAssignment3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int optionalNum;
+            Console.WriteLine("Please enter an integer: ");
+            int userNumber1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You may now enter another integer or nothing: ");
+            string userNumber2 = Console.ReadLine();
+            if (userNumber2 != "")
+            {
+                try
+                {
+                    optionalNum = Convert.ToInt32(userNumber2);
+                    Console.WriteLine("The results are: " + TakeTwo.Smash(userNumber1, optionalNum));
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Your did not enter an integer...");
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("The results are: " + TakeTwo.Smash(userNumber1));
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ClassAssignment3/ClassAssignment3/TakeTwo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment3
+{
+    class TakeTwo
+    {
+        public static int Smash(int userNumber1, int optionalNum = 0)
+        {
+            int op1 = Convert.ToInt32((Math.Pow(userNumber1, 2)) - userNumber1);
+            int op2 = Convert.ToInt32((Math.Pow(optionalNum, 2)) - optionalNum);
+            int result = op1 + op2;
+            return result;
+            
+        }
+    }
+}

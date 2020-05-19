0381e18a9d292e967204b03281ea383a0b342b52
tropnepod <59154286+tropnepod@users.noreply.github.com>
2020-05-19 14:15:29 -0700

    add class assignment 7 files

---
+++ ClassAssignment7/ClassAssignment6/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment6
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            // Console.WriteLine("This is coming from the Employee Class...");
+            base.SayName();
+        }
+        public int Id { get; set; }
+
+        public void Quit()
+        {
+            Console.WriteLine("Press enter to close the application: ");
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ ClassAssignment7/ClassAssignment6/IQuittable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment6
+{
+    interface IQuittable
+    {
+        void Quit();
+    }
+}

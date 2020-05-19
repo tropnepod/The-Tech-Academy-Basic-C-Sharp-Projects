8cf77b89d7d1d196207f35a80df77399c2230a67
tropnepod <59154286+tropnepod@users.noreply.github.com>
2020-05-19 11:39:58 -0700

    add class assignment 6 files

---
+++ ClassAssignment6/ClassAssignment6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee example = new Employee() { FirstName = "Sample", LastName = "Student" };
+            example.SayName();
+            Console.ReadLine();
+        }
+    }
+}

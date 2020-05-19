e1c1625984fa60976d5f40b4ca1513465807b3dc
tropnepod <59154286+tropnepod@users.noreply.github.com>
2020-05-18 17:50:38 -0700

    add class assignment 4 files

---
+++ ClassAssignment4/ClassAssignment4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TakeTwo example = new TakeTwo();
+            example.Marbles(12, 137);
+            example.Marbles(int2: 45, int1: 34);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ClassAssignment4/ClassAssignment4/TakeTwo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment4
+{
+    class TakeTwo
+    {
+        public void Marbles(int int1, int int2)
+        {
+            int1 = int1 * 23;
+            Console.WriteLine(int2.ToString());
+        }
+    }
+}

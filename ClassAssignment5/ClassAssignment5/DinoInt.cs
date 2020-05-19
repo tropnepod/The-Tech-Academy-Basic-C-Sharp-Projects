0de41eec03f210f84811b4c1309e3019a6b93fab
tropnepod <59154286+tropnepod@users.noreply.github.com>
2020-05-19 10:36:49 -0700

    add class assignment 5 files

---
+++ ClassAssignment5/ClassAssignment5/DinoInt.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment5
+{
+    public static class DinoInt
+    {
+        
+
+        public static void Mod1(int number, out int result)
+        {
+            result = number / 2;
+            Console.WriteLine("The result is: " + result);
+        }
+
+        public static void Mod2(double number, out double mod2Result, out string mod2Msg)
+        {
+            mod2Result = (number * 325) + (number / 128);
+            mod2Msg = "The result of the operation is: " + mod2Result.ToString();
+
+        }
+        
+        public static void Mod2(string entry, out string newString)
+        {
+            
+            char[] charEntry = entry.ToCharArray();
+
+            Array.Reverse(charEntry);
+            string tempString = new string(charEntry);
+            newString = tempString.ToUpper();
+            
+        }
+        
+    }
+}
---
+++ ClassAssignment5/ClassAssignment5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Please enter a number to be divided by two: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int result;
+            DinoInt.Mod1(number, out result);
+
+            Console.WriteLine("Please enter a numeric value: ");
+            double userInput = Convert.ToDouble(Console.ReadLine());
+            double mod2Result;
+            string mod2Msg;
+            DinoInt.Mod2(userInput, out mod2Result, out mod2Msg);
+            Console.WriteLine(mod2Msg);
+
+            Console.WriteLine("Enter a word or sentence: ");
+            string userInput2 = Console.ReadLine();
+            string newString;
+            DinoInt.Mod2(userInput2, out newString);
+            Console.WriteLine(newString);
+            Console.ReadLine();
+        }
+    }
+}

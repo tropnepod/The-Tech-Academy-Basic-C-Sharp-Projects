20ad86003cb4960656c39ae82b3dfe700b7a01f0
tropnepod <59154286+tropnepod@users.noreply.github.com>
2020-05-20 14:52:01 -0700

    add struct assignment files

---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number userNumber = new Number(); // instantiate 
+            Number userNumber2 = userNumber; 
+            userNumber.Amount = 13.8886468m; // set amount for userNumber
+            Console.WriteLine(userNumber.Amount); // print value of userNumber.Amount
+            Console.WriteLine(userNumber2.Amount); // print value of userNumber2.Amount to show setting one doesn't
+            Console.ReadLine();                     // affect the other.
+        }
+    }
+}

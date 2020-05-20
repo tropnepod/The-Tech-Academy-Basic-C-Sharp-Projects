129588a17aaff1e826f7141a8d6cb528a34862fb
tropnepod <59154286+tropnepod@users.noreply.github.com>
2020-05-19 19:27:27 -0700

    update class assignment 8 to the correct files

--- ClassAssignment8/ClassAssignment6/Employee.cs
+++ ClassAssignment8/ClassAssignment6/Employee.cs
@@ -14,12 +14,43 @@ namespace ClassAssignment6
             base.SayName();
         }
         public int Id { get; set; }
-
+        
+        
+        
         public void Quit()
         {
             Console.WriteLine("Press enter to close the application: ");
             Console.ReadLine();
             Environment.Exit(0);
+        }
+
+        public static Employee operator ==(Employee example, Employee example2)
+        {
+            if (example.Id == example2.Id)
+            {               
+                Console.WriteLine("These have matching ID numbers!");
+                return example2; // returns the duplicated entry, that can be removed
+            }
+            else
+            {
+                Console.WriteLine("These are NOT duplicate entries...");
+                return new Employee(); // will return empty object if not a duplicate
+            }
+            
+        }
+
+        public static Employee operator !=(Employee example, Employee example2)
+        {
+            if (example.Id == example2.Id)
+            {
+                Console.WriteLine("These have matching ID numbers!");
+                return example2; // returns duplicated entry
+            }
+            else if (example.Id != example2.Id)
+            {
+                Console.WriteLine("ID numbers do not match...");
+            }
+            return new Employee(); // will return empty object if not a duplicate
         }
     }
 }
--- ClassAssignment8/ClassAssignment6/Program.cs
+++ ClassAssignment8/ClassAssignment6/Program.cs
@@ -10,11 +10,24 @@ namespace ClassAssignment6
     {
         static void Main(string[] args)
         {
-            Employee example = new Employee() { FirstName = "Sample", LastName = "Student" };
+            Employee example = new Employee() { FirstName = "Sample", LastName = "Student", Id = 1};
+            Employee example2 = new Employee() { FirstName = "John", LastName = "Doe", Id = 2 };
+            Employee duplicate = new Employee();
+            
+            
             example.SayName();
+            example2.SayName();
+            duplicate = example==example2; // sets 'duplicate' value to example2 if it is a duplicate of example,
+                                            //  other will return an empty Employee object
+            if (duplicate.FirstName != null)
+            {
+                Console.WriteLine("Name: {0} {1}, ID: {2} is a duplicate employee number", duplicate.FirstName,duplicate.LastName, duplicate.Id);
+            }
+            
             Console.ReadLine();
-            IQuittable interfaceExample = new Employee();
-            interfaceExample.Quit();
+
+            //IQuittable interfaceExample = new Employee();
+            //interfaceExample.Quit();
         }
     }
 }

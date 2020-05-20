5ffceb8708c9f1f852963b8b2a061fe1a841f2a5
tropnepod <59154286+tropnepod@users.noreply.github.com>
2020-05-19 20:35:58 -0700

    add class assignment 9 files

---
+++ ClassAssignment9/ClassAssignment6/Employee.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment6
+{
+    public class Employee<T> : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            // Console.WriteLine("This is coming from the Employee Class...");
+            base.SayName();
+        }
+        public int Id { get; set; }
+
+        public List<T> things { get; set; }
+        
+        public void Quit()
+        {
+            Console.WriteLine("Press enter to close the application: ");
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+
+        public static Employee<T> operator ==(Employee<T> example, Employee<T> example2)
+        {
+            if (example.Id == example2.Id)
+            {               
+                Console.WriteLine("These have matching ID numbers!");
+                return example2; // returns the duplicated entry, that can be removed
+            }
+            else
+            {
+                Console.WriteLine("These are NOT duplicate entries...");
+                return new Employee<T>(); // will return empty object if not a duplicate
+            }
+            
+        }
+
+        public static Employee<T> operator !=(Employee<T> example, Employee<T> example2)
+        {
+            if (example.Id == example2.Id)
+            {
+                Console.WriteLine("These have matching ID numbers!");
+                return example2; // returns duplicated entry
+            }
+            else if (example.Id != example2.Id)
+            {
+                Console.WriteLine("ID numbers do not match...");
+            }
+            return new Employee<T>(); // will return empty object if not a duplicate
+        }
+    }
+}
---
+++ ClassAssignment9/ClassAssignment6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> example = new Employee<string>() { FirstName = "Sample", LastName = "Student", Id = 1, things = new List<string>() { "thing 1", "thing 2", "thing 3", "thing 4", "thing 5" } };
+            Employee<string> example2 = new Employee<string>() { FirstName = "John", LastName = "Doe", Id = 2 };
+            Employee<string> duplicate = new Employee<string>();
+            Employee<int> example3 = new Employee<int>() { things = new List<int>() { 1, 24, 53235, 235, 214, 643 } };
+                        
+            foreach (string thing in example.things)
+            {
+                Console.WriteLine("String list: " + thing.ToString());
+            }
+            foreach (int number in example3.things)
+            {
+                Console.WriteLine("Integer List: " + number.ToString());
+            }            
+
+            example.SayName();
+            example2.SayName();
+            duplicate = example==example2; // sets 'duplicate' value to example2 if it is a duplicate of example,
+                                            //  other will return an empty Employee object
+            if (duplicate.FirstName != null)
+            {
+                Console.WriteLine("Name: {0} {1}, ID: {2} is a duplicate employee number", duplicate.FirstName,duplicate.LastName, duplicate.Id);
+            }
+            
+            Console.ReadLine();
+
+            //IQuittable interfaceExample = new Employee();
+            //interfaceExample.Quit();
+        }
+    }
+}

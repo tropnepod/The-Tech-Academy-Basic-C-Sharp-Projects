b3cdf444ad98d2edfc8c97845148867ab914837c
tropnepod <59154286+tropnepod@users.noreply.github.com>
2020-05-19 18:29:12 -0700

    add class assignmnet 8 files

---
+++ ClassAssignment8/ClassAssignment6/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment6
+{
+    public abstract class Person
+    {
+        public virtual void SayName()
+        {
+            Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+        }
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+    }
+}

209e08d4b2ff97514fe2bb08c7a0fcf3c59e3844
tropnepod <59154286+tropnepod@users.noreply.github.com>
2020-05-20 11:56:15 -0700

    add enum assisgnment files

---
+++ EnumAssignment/EnumAssignment/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                
+                Console.WriteLine("Please enter the current day of the week (1-7): ");
+                int userInput = Convert.ToInt32(Console.ReadLine());
+                if (userInput > 0 && userInput < 8)
+                {
+                    WeekDay currentDay = (WeekDay)userInput;
+                    Console.WriteLine(currentDay);
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid day of the week...");
+                }
+                               
+            }
+            catch (FormatException X)
+            {
+                Console.WriteLine("Please enter a valid day of the week...");
+            }
+            catch (ArgumentException X)
+            {
+                Console.WriteLine(X.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+    public class Day
+    {
+        public WeekDay WeekDay { get; set; }
+    }
+    public enum WeekDay
+    {
+        Monday=1,
+        Tuesday=2,
+        Wednesday=3,
+        Thursday=4,
+        Friday=5,
+        Saturday=6,
+        Sunday=7
+    }
+}

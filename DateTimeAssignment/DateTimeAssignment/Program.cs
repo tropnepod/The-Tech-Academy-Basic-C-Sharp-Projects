71d33faae9bcfca7857bab7a32b51779bb481651
tropnepod <59154286+tropnepod@users.noreply.github.com>
2020-05-22 14:45:13 -0700

    add date time assignment files

---
+++ DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDateTime = DateTime.Now;
+            Console.WriteLine("The current date and time is: " + currentDateTime);
+            Console.WriteLine("Please enter a number...");
+            double number = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("In {0} hours, it will be {1}", number, currentDateTime.AddHours(number));
+            
+            Console.Read();
+        }
+    }
+}
